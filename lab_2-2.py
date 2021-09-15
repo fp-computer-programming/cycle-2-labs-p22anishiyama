bdc2e2924647f064e5fc69adc652a726ae67fee1
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-09-15 10:58:43 -0400

    submitting

---
+++ lab_2-2.py
@@ -0,0 +1,14 @@
+# Author: ATN 9/15/21
+
+a = 3
+b = a
+print(b)
+print(a)
+
+# Value of both variables are synonymous
+
+a = 7
+print(a)
+print(b)
+
+# Both the values are updated, yet the value of b is never altered
