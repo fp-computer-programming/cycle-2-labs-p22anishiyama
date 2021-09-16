5ada7d0042f26fc920194e74d8766e7b7146a6a6
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-09-16 10:13:26 -0400

    submitting

---
+++ lab_3-1.py
@@ -0,0 +1,17 @@
+# Author: ATN 9/16/21
+
+a = 2
+print(a)
+
+a = float(a)
+print(a)
+
+a = str(a)
+print(type(a))
+
+# Prints that 'a' is a String.
+
+a = bool(a)
+print(a == 2)
+
+# The result is printed as False because 'a' is a boolean value of True.
