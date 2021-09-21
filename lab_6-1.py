e62441611ed65067b9cfb38329bd29dd9aef902b
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-09-21 09:46:08 -0400

    submitting

---
+++ lab_6-1.py
@@ -0,0 +1,12 @@
+# Author: ATN 9/21/21
+
+# Inputs
+
+fname = input("Enter your first name:")
+
+lname = input("Enter your last name:")
+
+# Adding a space to make it legible
+
+fullname = fname + " " + lname
+print(fullname)
